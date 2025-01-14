70a4044b5407ad6e2a9e6eef498aac15200d5d7c
saurabhschheda <saurabh.s.chheda@gmail.com>
2025-01-14 13:33:28 +0530

    Removed streamlit

--- summarizer.py
+++ summarizer.py
@@ -2,18 +2,19 @@
 import PyPDF2
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
-from IPython.display import display, Markdown, clear_output
-import streamlit as st
+import argparse
 
 # Initialize the LLaMA model via Ollama
 model_id = "llama3.1"
 model = Ollama(model=model_id)
+
 
 # Function to extract text from PDF
 def extract_text_from_pdf(file):
     reader = PyPDF2.PdfReader(file)
     text = [reader.pages[page].extract_text() for page in range(len(reader.pages))]
     return text
+
 
 def summarize_text(input_text: str):
     """
@@ -40,35 +41,44 @@ def summarize_text(input_text: str):
     input_prompt = prompt.format(text=input_text)
 
     # Step 3: Generate the summary
-    display(Markdown("**Generating Summary...**"))
     result = model(input_prompt)
-    
-    # Display the final result
-    clear_output(wait=True)
-    display(Markdown(f"### Final Summary:\n\n{result.strip()}"))
-    
     return result.strip()
 
-# Streamlit UI
-st.title("PDF Summarizer with Llama3.1 Model")
 
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-
-if uploaded_file is not None:
-    # bytes_data = uploaded_file.getvalue()
-    # Extract text from uploaded PDF
-    with st.spinner('Extracting text from PDF...'):
-        document_text = extract_text_from_pdf(uploaded_file)
-
-    # Summarize the extracted text
-    if st.button("Summarize Text"):
-        with st.spinner('Generating Summary...'):
-            summary = ""
-            count = 0
+def main(pdf, output):
+    with open(pdf, "rb") as f:
+        print("Extracting text from PDF...")
+        document_text = extract_text_from_pdf(f)
+        document_size = len(document_text)
+        print(f"Extracted {document_size} pages from pdf")
+        count = 0
+        with open(output, "w", encoding="utf-8") as out:
             for page in document_text:
-                summary += summarize_text(page)
+                summary = summarize_text(page)
                 count += 1
-                st.markdown(f"Summarized {count} pages")
-        
-        st.subheader("Summary")
-        st.markdown(summary)
+                print(f"Page size: {len(page)}", f"Summary Size: {len(summary)}")
+                print(f"Summarized {count}/{document_size} pages")
+                out.write(f"Page {count}\n")
+                out.write(summary)
+                out.write('\n')
+        print("Done")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--pdf",
+        metavar="path",
+        required=False,
+        help="the path to pdf",
+        default="D:/OMSCS/06_HPC/Theory/Week00/1_IO_Complexity_of_Sorting_and_Related_Problems.pdf",
+    )
+    parser.add_argument(
+        "--output",
+        metavar="path",
+        required=False,
+        help="the path to output",
+        default="D:/OMSCS/06_HPC/Theory/Week00/1_IO_Complexity_of_Sorting_and_Related_Problems.txt",
+    )
+    args = parser.parse_args()
+    main(pdf=args.pdf, output=args.output)
